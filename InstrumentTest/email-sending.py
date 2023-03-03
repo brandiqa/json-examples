8eff837132e34150dfdbaefa3f57f42318c97ca2
Jeyapriya Thalamuthu <Jeyapriya.Thalamuthu@startupfarms.corp>
2023-03-03 20:02:18 +0530

    push

---
+++ InstrumentTest/email-sending.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+import os
+
+port = 465
+smtp_server = "smtp.gmail.com"
+USER_EMAIL = os.environ.get("USER_EMAIL")
+USER_PASSWORD = os.environ.get("USER_PASSWORD")
+
+message = """\
+    Subject: Welcome Ubaydah
+
+    This is your welcome email running 
+"""
+
+context = ssl.create_default_context()
+
+server = smtplib.SMTP_SSL(smtp_server, port, context=context)
+
+server.login(USER_EMAIL, USER_PASSWORD)
+server.sendmail(USER_EMAIL, USER_EMAIL, message)
